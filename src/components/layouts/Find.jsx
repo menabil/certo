6218b6d55afd118a0a505c2197b282db9a4f435a
menabil <nabil99cc@gmail.com>
2026-06-12 22:52:45 +0600

    responsive find section

--- src/components/layouts/Find.jsx
+++ src/components/layouts/Find.jsx
@@ -4,15 +4,15 @@ import Flex from "../Flex";
 
 const Find = () => {
   return (
-    <section className="bg-linear-to-b from-[#2925CC] to-[#4B48E5] py-[84px]">
+    <section className="bg-linear-to-b from-[#2925CC] to-[#4B48E5] md:py-[84px] py-15">
       <Container className={"text-center"}>
-        <h3 className="text-white text-[34px] font-extrabold">
+        <h3 className="text-white text-3xl md:w-full w-[280px] md:mx-0 mx-auto md:text-[34px] font-extrabold">
           Is someone spying on your phone?
         </h3>
-        <p className="text-white text-[18px] pt-[50px] pb-[60px]">
+        <p className="text-white text-[18px] py-10 md:pt-[50px] md:pb-[60px]">
           Find out with Certo
         </p>
-        <Flex className={"gap-x-6 justify-center"}>
+        <Flex className={"gap-6 flex-col md:flex-row justify-center"}>
           {/* Button */}
           <Button
             btnText={"Get Certo for iPhone"}
