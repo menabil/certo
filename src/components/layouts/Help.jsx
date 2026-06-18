85e54da26befe88e24c066574609f6b212a4726f
menabil <nabil99cc@gmail.com>
2026-06-18 23:31:48 +0600

    responsive help section

--- src/components/layouts/Help.jsx
+++ src/components/layouts/Help.jsx
@@ -7,19 +7,19 @@ const Help = () => {
   return (
     <section>
       <Container className={"py-[84px] px-9"}>
-        <Flex className={"justify-center"}>
-          <div className="rounded-[48px] bg-linear-to-b from-[#F7C95F] to-[#FDB235] p-12">
+        <Flex className={"justify-center flex-col md:flex-row gap-y-10"}>
+          <div className="rounded-[48px] bg-linear-to-b from-[#F7C95F] to-[#FDB235] md:p-12 p-7">
             <h5 className="font-extrabold text-[#02033B] text-2xl">
               Read our story
             </h5>
-            <p className="text-[16px] text-[#02033B] w-[360px] pt-6 pb-12">
+            <p className="text-[16px] text-[#02033B] md:w-[360px] w-[240px] pt-6 pb-12">
               Find out why thousands trust Certo to secure their mobile world.
             </p>
             <div className="bg-[#4335DE] rounded-[42px] w-[134px] px-6 py-3.5 text-white text-sm font-extrabold flex items-center">
               About us <FaArrowRightLong className="ml-2" />
             </div>
           </div>
-          <div className="pl-[72px]">
+          <div className="md:pl-[72px]">
             <h3 className="text-2xl text-[#02033B] font-extrabold">
               Help Center
             </h3>
