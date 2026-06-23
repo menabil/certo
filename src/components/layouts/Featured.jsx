42093a9195e80c78004e40483f29c0d2153aee0a
menabil <nabil99cc@gmail.com>
2026-06-23 23:17:07 +0600

    responsive featured section

--- src/components/layouts/Featured.jsx
+++ src/components/layouts/Featured.jsx
@@ -12,20 +12,20 @@ import { IoStar } from "react-icons/io5";
 const Featured = () => {
   return (
     <section className="bg-linear-to-b from-[#F7C95F] to-[#FDB235]">
-      <Container className={"py-[84px]"}>
-        <h3 className="text-4xl text-[#02033B] w-[565px] font-extrabold">
+      <Container className={"md:py-[84px] py-10"}>
+        <h3 className="md:text-4xl text-[26px] text-[#02033B] md:w-[565px] w-[285px] font-extrabold md:mx-0 md:text-left mx-auto text-center">
           Loved by thousands of iPhone and Android users alike
         </h3>
-        <Flex className="py-[72px] gap-x-7">
-          <div className="rounded-[48px] p-9 bg-white w-1/3 h-[336px]">
-            <div className="flex gap-x-1 text-[#FFC247] text-2xl">
+        <Flex className="md:py-[72px] py-10 gap-7 md:flex-row flex-col">
+          <div className="rounded-[48px] md:p-9 p-6 bg-white md:w-1/3 md:h-[336px] mx-4">
+            <div className="flex gap-x-1 text-[#FFC247] md:text-2xl">
               <IoStar />
               <IoStar />
               <IoStar />
               <IoStar />
               <IoStar />
             </div>
-            <p className="py-6  w-[320px]">
+            <p className="py-6 md:w-[320px]">
               Does exactly what it says. Clear to read and understand. This is
               now the second iPhone we’ve used it on and would certainly
               recommend this app.
@@ -34,15 +34,15 @@ const Featured = () => {
               colinandmandy94
             </h6>
           </div>
-          <div className="rounded-[48px] p-9 bg-white w-1/3 h-[336px]">
-            <div className="flex gap-x-1 text-[#FFC247] text-2xl">
+          <div className="rounded-[48px] md:p-9 p-6 bg-white md:w-1/3 md:h-[336px] mx-4">
+            <div className="flex gap-x-1 text-[#FFC247] md:text-2xl">
               <IoStar />
               <IoStar />
               <IoStar />
               <IoStar />
               <IoStar />
             </div>
-            <p className="py-6  w-[320px]">
+            <p className="py-6 md:w-[320px]">
               Love the ease and efficiency. Awesome app. Very informative and
               insightful if wanting to know more about your device. The added
               breach check.is a great bonus. Check any email of they’ve ever
@@ -53,15 +53,15 @@ const Featured = () => {
               Jennifer Black
             </h6>
           </div>
-          <div className="rounded-[48px] p-9 bg-white w-1/3 h-[336px]">
-            <div className="flex gap-x-1 text-[#FFC247] text-2xl">
+          <div className="rounded-[48px] md:p-9 p-6 bg-white md:w-1/3 md:h-[336px] mx-4">
+            <div className="flex gap-x-1 text-[#FFC247] md:text-2xl">
               <IoStar />
               <IoStar />
               <IoStar />
               <IoStar />
               <IoStar />
             </div>
-            <p className="py-6 w-[320px]">
+            <p className="py-6 md:w-[320px]">
               This app is good if you need to identify certain vulnerabilities
               on your iPhone. If you have any issues, their customer service was
               quite helpful and responsive.
@@ -72,11 +72,11 @@ const Featured = () => {
           </div>
         </Flex>
         {/* Bottom part */}
-        <Flex>
-          <p className="font-extrabold text-2xl text-[#02033B] pr-[75px]">
+        <Flex className={" flex-col md:flex-row"}>
+          <p className="font-extrabold text-2xl text-[#02033B] md:pr-[75px]">
             Featured in:
           </p>
-          <Flex className="py-10.5 gap-x-10">
+          <Flex className="py-10.5 gap-x-10 flex-col md:flex-row">
             <Images imgSrc={LogoOne} />
             <Images imgSrc={LogoTwo} />
             <Images imgSrc={LogoThr} />
