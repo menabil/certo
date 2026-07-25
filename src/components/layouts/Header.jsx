4909a7ed6fb06547e612292a514bb459529512cd
menabil <nabil99cc@gmail.com>
2026-07-26 00:16:26 +0600

    delete utils file

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,19 +1,37 @@
+import { useState } from "react";
 import Container from "../Container";
 import Flex from "../Flex";
 import Images from "../Images";
 import Logo from "/src/assets/logo.png";
+
+// React Icons
 import { FaChevronDown, FaBars, FaXmark } from "react-icons/fa6";
-import { useMobileMenu } from "../../utils/navigation";
+
+// Mobile menu toggling custom hook
+export const useMobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return { isOpen, toggleMenu, closeMenu };
+};
 
 const Header = () => {
-  const { isOpen, toggleMenu } = useMobileMenu();
+  // custom hook use kora hocche
+  const { isOpen, toggleMenu, closeMenu } = useMobileMenu();
 
   return (
     <section className="bg-[#F3F8FF] relative">
       <Container>
-        <Flex className="py-4 md:py-6 items-center justify-between px-2 md:px-0">
+        <Flex className="py-4 md:py-6 items-center justify-between">
           {/* Logo Section */}
-          <div className="w-32 sm:w-36">
+          <div className="w-32 sm:w-36 pl-3 sm:pl-0">
             <Images imgSrc={Logo} className="w-full h-auto" />
           </div>
 
@@ -33,16 +51,28 @@ const Header = () => {
           >
             {/* Menu Links */}
             <ul className="flex flex-col lg:flex-row items-center gap-y-4 lg:gap-y-0 lg:gap-x-8 xl:gap-x-12 w-full lg:w-auto text-center">
-              <li className="font-extrabold text-base xl:text-xl text-[#02033B] hover:text-[#4335DE] cursor-pointer transition-colors">
+              <li
+                onClick={closeMenu}
+                className="font-extrabold text-base xl:text-xl text-[#02033B] hover:text-[#4335DE] cursor-pointer transition-colors"
+              >
                 iPhone
               </li>
-              <li className="font-extrabold text-base xl:text-xl text-[#02033B] hover:text-[#4335DE] cursor-pointer transition-colors">
+              <li
+                onClick={closeMenu}
+                className="font-extrabold text-base xl:text-xl text-[#02033B] hover:text-[#4335DE] cursor-pointer transition-colors"
+              >
                 Android
               </li>
-              <li className="font-extrabold text-base xl:text-xl text-[#02033B] hover:text-[#4335DE] cursor-pointer transition-colors">
+              <li
+                onClick={closeMenu}
+                className="font-extrabold text-base xl:text-xl text-[#02033B] hover:text-[#4335DE] cursor-pointer transition-colors"
+              >
                 Help
               </li>
-              <li className="font-extrabold text-base xl:text-xl text-[#02033B] hover:text-[#4335DE] cursor-pointer transition-colors flex items-center justify-center gap-x-2">
+              <li
+                onClick={closeMenu}
+                className="font-extrabold text-base xl:text-xl text-[#02033B] hover:text-[#4335DE] cursor-pointer transition-colors flex items-center justify-center gap-x-2"
+              >
                 Company
                 <FaChevronDown className="text-sm xl:text-base" />
               </li>
@@ -50,6 +80,7 @@ const Header = () => {
 
             {/* Action Button */}
             <button
+              onClick={closeMenu}
               className="w-full sm:w-auto font-extrabold lg:ml-6 xl:ml-12 py-2.5 px-6 xl:py-3.5 xl:px-8 rounded-full bg-[#4335DE] text-white border-2 border-[#4335DE] hover:bg-transparent hover:text-[#4335DE] duration-300 cursor-pointer text-sm xl:text-base"
             >
               Sign in
